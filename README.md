4e94d632728056dc6cce8fc1f44867b21bdba176
Joey Dye <joey.m.dye@gmail.com>
2018-02-18 20:48:06 -0600

    doc: Update README with new gameplay rules

--- README.md
+++ README.md
@@ -4,9 +4,8 @@
 
 To get started with Bug Run, simply visit the following URL:
 
-You can also clone the project to your local computer and run the project on your local host.
+You can also clone the project to your local computer and run it on your local host.
 
 ## Gameplay
 
-Start the game by selecting a player. To play, use the up, down, right, and left arrow keys to move the player across the screen. Your goal is to aquire points by getting your player to reach the river without being hit by and enemy. Your player starts out with 3 lives. For each time the player collides with an enemy bug, a life is removed. When all three lives are gone, the game restarts. See how many points you can get before all your lives run out.
-
+Start the game by selecting a player. To play, use the up, down, right, and left arrow keys to move the player across the screen. Your goal is to get the player to the star without being hit by an enemy. If your player collides with an enemy, he/she will revert back to the starting position. Once your player reaches the star, you win.
--- js/app.js
+++ js/app.js
@@ -1,95 +1,89 @@
-// Open modal on load
+// Open character select modal on load
 $('#myModal').modal('show')
 
 // Enemies our player must avoid
 class Enemy {
   constructor(x, y) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.x = x; // added for start x position
-    this.y = y; // added for start y position
-    this.speed = Math.floor(Math.random() * (15 - 10) + 10);
+    this.x = x;
+    this.y = y;
+    this.speed = Math.floor(Math.random() * (15 - 10) + 10); // Give each enemy a random speed
     this.sprite = 'images/enemy-bug.png';
   }
 
-  // Update the enemy's position, required method for game
-  // Parameter: dt, a time delta between ticks
+  // Multiply any movement by the dt parameter to ensure the game runs at the same speed for
+  // all computers.
   update(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
     this.speed * dt;
   }
 
-  // Draw the enemy on the screen, required method for game
+  // Draw enemy on the screen
   render() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
   }
 
-  // Check enemy player collisions
-
+  // Check for enemy player collisions and restart player on collision
   checkCollisions() {
     if (this.x > (player.x - 65) && this.x < player.x + 80 && this.y >= (player.y - 70) && this.y < player.y + 65) {
-    player.resetPlayer(); // Reset player to start position
+    player.resetPlayer();
      }
    }
 
-    // If enemy off screen reset position
+  // If enemy off screen reset position
+  reset() {
+    if (this.x > 515) {
+      this.x = -200;}
+  }
 
-    reset() {
-      if (this.x > 515) {
-        this.x = -200;}
-    }
-
-    moveEnemy(enemy) {
-      const enemyInterval = setInterval(() => {
-        if (player.winGame === true) {
-          clearInterval(enemyInterval);
-        }
-        enemy.checkCollisions();
-        enemy.update();
-        enemy.reset();
-        enemy.x += enemy.speed;
-        }, 300)};
+  // Move enemy across screen
+  moveEnemy(enemy) {
+    const enemyInterval = setInterval(() => {
+      // Stop enemies on game win
+      if (player.winGame === true) {
+        clearInterval(enemyInterval);
+      }
+      enemy.checkCollisions();
+      enemy.update();
+      // Reset enemy if it reaches end of screen
+      enemy.reset(); 
+      enemy.x += enemy.speed;
+      }, 75)};
 } // End of Enemy class
-
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
 
 class Player {
   constructor() {
     this.x = 200;
     this.y = 380;
-    this.speed = 20;
+    this.speed = 30;
     this.sprite;
     this.winGame = false;
   }
 
+  // Multiply any movement by the dt parameter to ensure the game runs at the same speed for
+  // all computers.
   update(dt) {
     this.speed * dt;
   }
 
+  // Load player on screen
   render() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
   }
 
   // Reset player to start position
-
   resetPlayer() {
     this.x = 200;
     this.y = 420;
   }
 
+  // Handle game win
   win() {
-    const winMessage = document.querySelector('.win-message');
-    winMessage.innerHTML = 'You Win!!!'
-    star.y = -200;
     this.winGame = true;
+    const winMessage = document.querySelector('.win-message'); // Display win message
+    winMessage.innerHTML = 'You Win!!!'
+    // Move star off screen
+    star.y = -200;
 
+    // Resent game after 3 seconds
     setTimeout(
       () => {
         this.resetPlayer()
@@ -100,18 +94,22 @@ class Player {
       }, 3000)
     }
 
+  // Enable player movement on keypress
   handleInput(keycode) {
+    // Stop player if game has been won
     if (player.winGame === true) return;
 
+    // Run win method if player reaches star
     if (this.x > (star.x - 70) && this.x < star.x + 60 && this.y >= (star.y - 50) && this.y < star.y + 50) {
       this.win();
     }
     this.update();
 
+    // Check keycode. Move player the correct direction. Stop movement if player reaches edge of screen.
     switch (keycode) {
       case 'up':
         if (this.y > -10) {
-        this.y -= this.speed;} // Test if player has reached screen edge
+        this.y -= this.speed;}
         break;
       case 'down':
         if (this.y < canvas.height - 170) {
@@ -135,57 +133,53 @@ class Player {
       this.sprite = 'images/star.png';
     }
 
+    // Load star on screen
     render() {
       ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
+  } // End of Star class
+
+// Add event listener to listen for player selection. Start game once player selected.
+const selectGirl = document.querySelector('#girl');
+const selectBoy = document.querySelector('#boy');
+
+let sprite;
+
+const setPlayerGirl = sprite => {
+    player.sprite = 'images/char-cat-girl.png';
+    $('#myModal').modal('hide');
+    Engine(this);
   }
 
-  // Select player
-  const selectGirl = document.querySelector('#girl');
-  const selectBoy = document.querySelector('#boy');
+const setPlayerBoy = sprite => {
+    player.sprite = 'images/char-boy.png';
+    $('#myModal').modal('hide');
+    Engine(this);
+  }
 
-  let sprite;
+selectGirl.addEventListener('click', setPlayerGirl);
+selectBoy.addEventListener('click', setPlayerBoy);
 
-  const setPlayerGirl = sprite => {
-      player.sprite = 'images/char-cat-girl.png';
-      $('#myModal').modal('hide');
-      Engine(this);
-    }
-
-  const setPlayerBoy = sprite => {
-      player.sprite = 'images/char-boy.png';
-      $('#myModal').modal('hide');
-      Engine(this);
-    }
-
-  selectGirl.addEventListener('click', setPlayerGirl);
-  selectBoy.addEventListener('click', setPlayerBoy);
-
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+// instantiate all enemy, player, and star objects, and place all enemies in array called allEnemies
 const player = new Player();
 const star = new Star();
 const enemy1 = new Enemy(-10, 60);
 const enemy2 = new Enemy(-10, 140);
 const enemy3 = new Enemy(-10, 220);
-const enemy4 = new Enemy(-350, 60);
-const enemy5 = new Enemy(-350, 140);
-const enemy6 = new Enemy(-350, 220);
+const enemy4 = new Enemy(-500, 60);
+const enemy5 = new Enemy(-500, 140);
+const enemy6 = new Enemy(-500, 220);
 const allEnemies = [enemy1, enemy2, enemy3, enemy4, enemy5, enemy6];
 
 // Move enemies at varying speed accross screen
-
 const startEnemies = () => {
   for (enemy of allEnemies) {
     enemy.moveEnemy(enemy);
   };
 }
-
 startEnemies();
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+// Listen for key presses and send the keys to Player.handleInput() method
 document.addEventListener("keyup", function(e) {
   var allowedKeys = {
     37: "left",
